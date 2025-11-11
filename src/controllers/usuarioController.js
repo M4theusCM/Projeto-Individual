c2e6a8ca567bdbe9d10ad22fc9ab88159e0e4c4d
M4theusCM <matheus.menezes@sptech.school>
2025-11-10 22:12:42 -0300

    Cadastro sem validar email, tell ou nick unicos

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -56,25 +56,26 @@ function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
-    var cpf = req.body.cpfServer;
+    var tell = req.body.tellServer;
+    var nick = req.body.nickServer;
+    var dtNasc = req.body.dtNascServer;
     var senha = req.body.senhaServer;
-    var fkEmpresa = req.body.idEmpresaVincularServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
-    } else if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
-    } else if (cpf == undefined) {
+    } else if (email == undefined && tell == undefined) {
+        res.status(400).send("Seu identificador está undefined!");
+    } else if (nick == undefined) {
         res.status(400).send("Seu CPF está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (fkEmpresa == undefined) {
+    } else if (dtNasc == undefined) {
         res.status(400).send("Sua empresa a vincular está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, cpf,senha, fkEmpresa)
+        usuarioModel.cadastrar(nome, email, tell, nick, dtNasc, senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
---
+++ src/database/dbgear.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE dbgear;
+USE dbgear;
+
+CREATE TABLE usuario(
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
+    email VARCHAR(50) UNIQUE,
+    tell CHAR(11) UNIQUE,
+    nickName VARCHAR(45) NOT NULL UNIQUE,
+    senha VARCHAR(45),
+    dtNasc DATE,
+    statusUser TINYINT,
+		CONSTRAINT chkStatus CHECK(statusUser IN (1,2)),
+	descricao VARCHAR(300),
+    imgPerfil VARCHAR(255)
+);
